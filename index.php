df30b164e037045f35be262d48292f722f2b1b91
dennisdalmolen <dennisssdalmolennn@gmail.com>
2022-10-12 01:28:56 +0200

    Index Changes

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="responsive.css">
-    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
     <title>De Roset</title>
 
 </head>
@@ -52,13 +52,53 @@
                     <h2>Smaak Van de Dag</h2>
                 </div>
                 <div class="ijs-image">
-
+                    <img src="images/ijs-fotos/ChocolateMarshmallow-300x258.jpg" alt="">
                 </div>
+                <div class="text-1">
+                    <h3>Chocolade</h3>
+                </div>
+                <a href="#">
+                    <div class="bestel-knop-1">
+                        Bestel
+                    </div>
+                </a>
             </div>
 
             <div class="populaire-smaken">
-
+                <div class="main-text">
+                    <h2>Populaire Smaken</h2>
+                </div>
+                <div class="ijs-image">
+                    <img src="images/ijs-fotos/PremiumAmarettoPeachCharlotte.jpg" alt="">
+                </div>
+                <div class="text-1">
+                    <h4>Amaretto Perzik</h4>
+                </div>
+                <div class="ijs-image">
+                    <img src="images/ijs-fotos/PremiumBananaPecan-300x258.jpg" alt="">
+                </div>
+                <div class="text-1">
+                    <h4>Pecan Banaan</h4>
+                </div>
+                <div class="ijs-image">
+                    <img src="images/ijs-fotos/PremiumMintChocolateChip-300x258.jpg" alt="">
+                </div>
+                <div class="text-1">
+                    <h4>Mint Chocolade Chip</h4>
+                </div>
             </div>
+        </div>
+
+        <div class="main-right">
+
+            <div class="main-main">
+                <div class="main-overons">
+                    <div class="main-text">
+                        <h2>Over Ons</h2>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
     </div>
