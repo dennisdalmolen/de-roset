ad362628839aba81f880d3e2138ddf92d946bdc2
dennisdalmolen <dennisssdalmolennn@gmail.com>
2022-10-11 21:43:53 +0200

    Header toegevoegd

--- index.php
+++ index.php
@@ -2,18 +2,69 @@
 <html lang="en">
 
 <head>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="responsive.css">
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
     <title>De Roset</title>
+
 </head>
 
 <header>
+
+    <div class="header-group">
+        <div class="header-left">
+            <img src="images/rosetlogo.png" alt="">
+        </div>
+        <div class="header-right">
+            <nav>
+                <a href="#">
+                    <div class="menu-text-active">Over Ons</div>
+                </a>
+                <a href="#">
+                    <div class="menu-text">Bestellen</div>
+                </a>
+                <a href="#">
+                    <div class="menu-text">Blog</div>
+                </a>
+                <a href="#">
+                    <div class="menu-text">Contact</div>
+                </a>
+                <a href="#">
+                    <div class="menu-text">Winkelmandje</div>
+                </a>
+            </nav>
+        </div>
+    </div>
 
 </header>
 
 <body>
 
+    <div class="main-group">
+
+        <div class="main-left">
+            <div class="smaak-vdd">
+                <div class="main-text">
+                    <h2>Smaak Van de Dag</h2>
+                </div>
+                <div class="ijs-image">
+
+                </div>
+            </div>
+
+            <div class="populaire-smaken">
+
+            </div>
+        </div>
+
+    </div>
+
 </body>
+
+<footer>
+
+</footer>
