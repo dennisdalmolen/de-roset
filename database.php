f4726a65802be71db704ea91622e907b32a448bf
dennisdalmolen <dennisssdalmolennn@gmail.com>
2022-10-11 13:33:14 +0200

    Database Geconfigureerd

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php
+
+// Database configuratie
+$host  = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "roset";
+
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
