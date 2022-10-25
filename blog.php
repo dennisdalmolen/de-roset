53face2f838130da930fc5dfd7709e6ed5796d72
dennisdalmolen <dennisssdalmolennn@gmail.com>
2022-10-25 04:08:36 +0200

    Blog Page Added

---
+++ blog.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+    <title>De Roset</title>
+
+</head>
+
+<header>
+
+    <div class="header-group">
+        <div class="header-left">
+            <img src="images/rosetlogo.png" alt="">
+        </div>
+        <div class="header-right">
+            <nav>
+                <a href="index.php">
+                    <div class="menu-text">Over Ons</div>
+                </a>
+                <a href="bestellen.php">
+                    <div class="menu-text">Bestellen</div>
+                </a>
+                <a href="blog.php">
+                    <div class="menu-text-active">Blog</div>
+                </a>
+                <a href="contact.php">
+                    <div class="menu-text">Contact</div>
+                </a>
+                <a href="winkelmandje.php">
+                    <div class="menu-text">Winkelmandje</div>
+                </a>
+            </nav>
+        </div>
+    </div>
+
+</header>
+
+<body>
+
+    <div class="main-group">
+
+        <div class="main-left">
+            <div class="smaak-vdd">
+                <div class="main-text">
+                    <h2>Smaak Van de Dag</h2>
+                </div>
+                <div class="ijs-image">
+                    <img src="images/ijs-fotos/ChocolateMarshmallow-300x258.jpg" alt="">
+                </div>
+                <div class="text-1">
+                    <h3>Chocolade</h3>
+                </div>
+                <a href="#">
+                    <div class="bestel-knop-1">
+                        Bestel
+                    </div>
+                </a>
+            </div>
+
+            <div class="populaire-smaken">
+                <div class="main-text">
+                    <h2>Populaire Smaken</h2>
+                </div>
+                <div class="ijs-image">
+                    <img src="images/ijs-fotos/PremiumAmarettoPeachCharlotte.jpg" alt="">
+                </div>
+                <div class="text-1">
+                    <h4>Amaretto Perzik</h4>
+                </div>
+                <div class="ijs-image">
+                    <img src="images/ijs-fotos/PremiumBananaPecan-300x258.jpg" alt="">
+                </div>
+                <div class="text-1">
+                    <h4>Pecan Banaan</h4>
+                </div>
+                <div class="ijs-image">
+                    <img src="images/ijs-fotos/PremiumMintChocolateChip-300x258.jpg" alt="">
+                </div>
+                <div class="text-1">
+                    <h4>Mint Chocolade Chip</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="main-right">
+
+            <div class="main-main">
+                <div class="main-overons">
+                    <div class="main-text">
+                        <h2>Blog</h2>
+                    </div>
+
+                    <div class="blog-article-1">
+                        <article>
+                            <div class="article-left-1">
+                                <div class="article-left-in">
+                                    <div class="main-text">
+                                        <h2>De Roset</h2>
+                                    </div>
+                                    <h5>
+                                        De Roset IJssalon is geopend in het zomerseizoen.
+                                        Je hebt keuze uit diverse smaken en toppings.
+                                        Voor ons banketbakkersijs wordt dagelijks verse melk bij de boer gehaald, dit vormt de basis van het heerlijke, ambachtelijk gemaakt ijs.
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="article-right-1">
+                                <div class="article-image-1">
+                                    <img src="images/ijszaak.jpg" alt="">
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+
+                    <div class="blog-article-2">
+                        <article>
+                            <div class="article-left-1">
+                                <div class="article-left-in">
+                                    <div class="main-text">
+                                        <h2>De Roset</h2>
+                                    </div>
+                                    <h5>
+                                        De Roset IJssalon is geopend in het zomerseizoen.
+                                        Je hebt keuze uit diverse smaken en toppings.
+                                        Voor ons banketbakkersijs wordt dagelijks verse melk bij de boer gehaald, dit vormt de basis van het heerlijke, ambachtelijk gemaakt ijs.
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="article-right-1">
+                                <div class="article-image-1">
+                                    <img src="images/ijszaak.jpg" alt="">
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+
+<footer>
+
+    <div class="footer-group">
+        <div class="footer-left">
+            <div class="footer-text">
+                <h6>Ons adress</h6>
+                <h7>Overtoom 17</h7>
+                <h7>1901 EP Castricum</h7>
+                <h7>Noord-Holland Nederland</h7>
+            </div>
+        </div>
+
+        <div class="footer-middle">
+            <h6>Wij bezorgen in:</h6>
+            <h7>Castricum</h7>
+            <h7>Uitgeest</h7>
+            <h7>Heemskerk</h7>
+        </div>
+
+        <div class="footer-right">
+            <a href="#">
+                Onze Voorwaarden
+            </a>
+        </div>
+    </div>
+
+</footer>
